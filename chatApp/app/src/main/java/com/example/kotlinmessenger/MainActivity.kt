cce7a7350c24b62d468ac90929d35005464e3ae7
onxmoreplz <hamdoyoung95@gmail.com>
2021-07-11 23:59:04 +0900

    [Add] Bind widgets in MainActivity

--- chatApp/app/src/main/java/com/example/kotlinmessenger/MainActivity.kt
+++ chatApp/app/src/main/java/com/example/kotlinmessenger/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.kotlinmessenger
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val email = email_edittext_register.text.toString()
+        val password = password_edittext_register.text.toString()
+
+        register_button_register.setOnClickListener {
+
+        }
+
+        already_have_account_text_view.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
