c675f581f8f56ad1fe3f3f0e0a0aa6487de362fc
onxmoreplz <hamdoyoung95@gmail.com>
2021-07-13 21:26:02 +0900

    [Refac] Rename MainActivity to RegisterActivity

---
+++ chatApp/app/src/main/java/com/example/kotlinmessenger/RegisterActivity.kt
@@ -0,0 +1,106 @@
+package com.example.kotlinmessenger
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import com.example.kotlinmessenger.dto.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_register.*
+import java.util.*
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        register_button_register.setOnClickListener {
+            performRegister()
+        }
+
+        already_have_account_text_view.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        selectphoto_button_register.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 0)
+        }
+    }
+
+    var selectedPhotoUri: Uri? = null
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if( requestCode == 0 && resultCode == Activity.RESULT_OK && data != null) {
+            Log.d("RegisterActivity", "Photo was selected")
+
+            selectedPhotoUri = data.data
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedPhotoUri)
+            val bitmapDrawable = BitmapDrawable(bitmap)
+            selectphoto_button_register.setBackgroundDrawable(bitmapDrawable)
+        }
+    }
+
+    private fun performRegister() {
+        val email = email_edittext_register.text.toString()
+        val password = password_edittext_register.text.toString()
+        Log.d("MainActvity", "Email : ${email}, password : $password")
+
+        if( email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "No Empty Email, Password", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        Firebase.auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener{
+                if(!it.isSuccessful) return@addOnCompleteListener
+
+                Log.d("MainActivity", "Successfully created user with uid : ${it.result?.user?.uid}")
+
+                uploadImageToFirebaseStorage()
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Failed to Create User : ${it.message}", Toast.LENGTH_SHORT).show()
+                Log.d("MainActivity", "Failed to Create User : ${it.message}")
+            }
+    }
+
+    private fun uploadImageToFirebaseStorage() {
+        if(selectedPhotoUri == null) return
+
+        val fileName = UUID.randomUUID().toString()
+        val ref = FirebaseStorage.getInstance().getReference("/iamges/$fileName")
+
+        ref.putFile(selectedPhotoUri!!)
+            .addOnSuccessListener {
+                ref.downloadUrl.addOnSuccessListener {
+                    saveUserToFirebaseDatabase(it.toString())
+                }
+            }
+
+    }
+
+    private fun saveUserToFirebaseDatabase(profileImageUrl: String) {
+        val uid = FirebaseAuth.getInstance().uid ?: ""
+        val ref = FirebaseDatabase.getInstance().getReference("users/$uid")
+
+        val user = User(uid, username_edittext_register.text.toString(), profileImageUrl)
+
+        ref.setValue(user)
+            .addOnSuccessListener {
+
+            }
+    }
+}
