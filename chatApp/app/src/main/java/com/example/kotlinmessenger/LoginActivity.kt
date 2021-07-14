1660723fec2e374d1af1def1d254aa07bb1e9fab
onxmoreplz <hamdoyoung95@gmail.com>
2021-07-14 22:59:31 +0900

    [Add] Make Button to back to RegisterActivity

--- chatApp/app/src/main/java/com/example/kotlinmessenger/LoginActivity.kt
+++ chatApp/app/src/main/java/com/example/kotlinmessenger/LoginActivity.kt
@@ -2,10 +2,15 @@ package com.example.kotlinmessenger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        tv_back_to_registeration.setOnClickListener {
+            finish()
+        }
     }
 }
