f801abf07c852aeb35b9e4663ab31b75469b976c
onxmoreplz <hamdoyoung95@gmail.com>
2021-07-16 01:09:59 +0900

    [Add] Fetch User Data in FirebaseDatabase

--- chatApp/app/src/main/java/com/example/kotlinmessenger/NewMessageActivity.kt
+++ chatApp/app/src/main/java/com/example/kotlinmessenger/NewMessageActivity.kt
@@ -1,7 +1,17 @@
 package com.example.kotlinmessenger
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kotlinmessenger.dto.User
+import com.example.kotlinmessenger.dto.UserItem
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.ViewHolder
 import kotlinx.android.synthetic.main.activity_new_message.*
 
 class NewMessageActivity : AppCompatActivity() {
@@ -11,9 +21,38 @@ class NewMessageActivity : AppCompatActivity() {
 
         supportActionBar?.title = "Select User"
 
+//        val adapter = GroupAdapter<ViewHolder>()
+//
+//        adapter.add(UserItem())
+//        adapter.add(UserItem())
+//        adapter.add(UserItem())
+//
+//        recyclerview_newmessage.adapter = adapter
 
+        fetchUsers()
+    }
 
-        recyclerview_newmessage.adapter
-        recyclerview_newmessage
+    private fun fetchUsers() {
+        val ref = FirebaseDatabase.getInstance().getReference("/users")
+        ref.addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val adapter = GroupAdapter<ViewHolder>()
+
+                snapshot.children.forEach{
+                    Log.d("NewMessageActivity", it.toString())
+                    var user = it.getValue(User::class.java)
+                    if(user != null) {
+                        adapter.add(UserItem(user))
+                    }
+                }
+
+                recyclerview_newmessage.adapter = adapter
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
     }
 }
