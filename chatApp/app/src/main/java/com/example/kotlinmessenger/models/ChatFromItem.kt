5d67f3b454f5accdea93429395cc003def616b2f
onxmoreplz <hamdoyoung95@gmail.com>
2021-07-19 22:10:45 +0900

    [Add] Add Text Param in ChatFromToItem Class

--- chatApp/app/src/main/java/com/example/kotlinmessenger/models/ChatFromItem.kt
+++ chatApp/app/src/main/java/com/example/kotlinmessenger/models/ChatFromItem.kt
@@ -3,10 +3,11 @@ package com.example.kotlinmessenger.models
 import com.example.kotlinmessenger.R
 import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.chat_from_row.view.*
 
-class ChatFromItem: Item<ViewHolder>() {
+class ChatFromItem(val text: String): Item<ViewHolder>() {
     override fun bind(viewHolder: ViewHolder, position: Int) {
-        //
+        viewHolder.itemView.textview_from_row.text = text
     }
 
     override fun getLayout(): Int {
--- chatApp/app/src/main/java/com/example/kotlinmessenger/models/ChatToItem.kt
+++ chatApp/app/src/main/java/com/example/kotlinmessenger/models/ChatToItem.kt
@@ -3,10 +3,11 @@ package com.example.kotlinmessenger.models
 import com.example.kotlinmessenger.R
 import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.chat_to_row.view.*
 
-class ChatToItem: Item<ViewHolder>() {
+class ChatToItem(val text: String): Item<ViewHolder>() {
     override fun bind(viewHolder: ViewHolder, position: Int) {
-        //
+        viewHolder.itemView.textview_to_row.text = text
     }
 
     override fun getLayout(): Int {
