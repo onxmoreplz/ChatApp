4c498fbf75bf8afbc6add0ce62dd33c5acc070e5
onxmoreplz <hamdoyoung95@gmail.com>
2021-07-20 22:03:35 +0900

    [Add] Load Profile Image using Picasso Library

--- chatApp/app/src/main/java/com/example/kotlinmessenger/models/ChatFromItem.kt
+++ chatApp/app/src/main/java/com/example/kotlinmessenger/models/ChatFromItem.kt
@@ -1,13 +1,20 @@
 package com.example.kotlinmessenger.models
 
 import com.example.kotlinmessenger.R
+import com.squareup.picasso.Picasso
 import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
 import kotlinx.android.synthetic.main.chat_from_row.view.*
 
-class ChatFromItem(val text: String): Item<ViewHolder>() {
+class ChatFromItem(val text: String, val user: User): Item<ViewHolder>() {
     override fun bind(viewHolder: ViewHolder, position: Int) {
         viewHolder.itemView.textview_from_row.text = text
+
+        val uri = user.profileImageUrl
+        val targetImageView = viewHolder.itemView.imageview_chat_from_row
+        Picasso.get()
+            .load(uri)
+            .into(targetImageView)
     }
 
     override fun getLayout(): Int {
--- chatApp/app/src/main/java/com/example/kotlinmessenger/models/ChatToItem.kt
+++ chatApp/app/src/main/java/com/example/kotlinmessenger/models/ChatToItem.kt
@@ -1,13 +1,20 @@
 package com.example.kotlinmessenger.models
 
 import com.example.kotlinmessenger.R
+import com.squareup.picasso.Picasso
 import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
 import kotlinx.android.synthetic.main.chat_to_row.view.*
 
-class ChatToItem(val text: String): Item<ViewHolder>() {
+class ChatToItem(val text: String, val user: User): Item<ViewHolder>() {
     override fun bind(viewHolder: ViewHolder, position: Int) {
         viewHolder.itemView.textview_to_row.text = text
+
+        val uri = user.profileImageUrl
+        val targetImageView = viewHolder.itemView.imageview_chat_to_row
+        Picasso.get()
+            .load(uri)
+            .into(targetImageView)
     }
 
     override fun getLayout(): Int {
